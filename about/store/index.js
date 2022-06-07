655fb0f2c76e0e0e4346b64973849796431769ed
JosTheDude <barnav.mitra@icloud.com>
2022-06-07 09:00:05 -0400

    Update

---
+++ about/store/index.js
@@ -0,0 +1,4 @@
+// force user to redirect to the site https://josnetwork.tebex.io if they visit the website
+if (window.location.protocol != "https:") {
+    window.location.href = "https://josnetwork.tebex.io";
+}
